721871f067a4b8ea76a5090c4d94b397822eaa28
m208 <avkomissarov@gmail.com>
2024-02-29 00:59:56 +0300

    feat: add text messages

--- src/constants/messageText.ts
+++ src/constants/messageText.ts
@@ -1,3 +1,8 @@
-export const NO_ORDERS_MESSAGE = 'No new orders';
+export const NO_ORDERS_MESSAGE = 'Новых заказов нет';
+export const NO_QUESTIONS_MESSAGE = 'Новых вопросов нет';
+export const NO_FEEDBACKS_MESSAGE = 'Новых отзывов нет';
+export const NEW_QUESTIONS_COUNT_MESSAGE = 'Неотвеченных вопросов: ';
+export const NEW_FEEDBACKS_COUNT_MESSAGE = 'Необработанных отзывов: ';
+
 export const LINE_DIVIDER_TG = '%0A';
 export const LINE_DIVIDER_WEB = ' | ';
