4c8ef2881a9cec49572a7f78c91e6c978ce283c6
m208 <avkomissarov@gmail.com>
2024-02-29 01:01:49 +0300

    feat: add new questions tracker to sheduler

--- src/scheduler/scheduler.module.ts
+++ src/scheduler/scheduler.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { SchedulerService } from './scheduler.service';
 import { NewOrdersTrackerModule } from 'src/new-orders-tracker/new-orders-tracker.module';
 import { TokenExpiresTrackerModule } from 'src/token-expires-tracker/token-expires-tracker.module';
+import { NewQuestionsModule } from 'src/new-questions/new-questions.module';
 
 @Module({
-  imports: [NewOrdersTrackerModule, TokenExpiresTrackerModule],
+  imports: [
+    NewOrdersTrackerModule,
+    TokenExpiresTrackerModule,
+    NewQuestionsModule,
+  ],
   providers: [SchedulerService],
 })
 export class SchedulerModule {}
--- src/scheduler/scheduler.service.ts
+++ src/scheduler/scheduler.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { NewOrdersTrackerService } from 'src/new-orders-tracker/new-orders-tracker.service';
+import { NewQuestionsService } from 'src/new-questions/new-questions.service';
 import { TokenExpiresTrackerService } from 'src/token-expires-tracker/token-expires-tracker.service';
 
 @Injectable()
@@ -9,12 +10,19 @@ export class SchedulerService {
   constructor(
     private readonly trackingService: NewOrdersTrackerService,
     private readonly tokenService: TokenExpiresTrackerService,
+    private readonly questionsService: NewQuestionsService,
   ) {}
 
   @Cron('0 1 * * * *')
   //  Called every hour, at the start of the 1th minute
   trackOrders() {
     this.trackingService.checkNewOrders();
+  }
+
+  @Cron('0 2 * * * *')
+  //  Called every hour, at the start of the 2nd minute
+  trackQuestions() {
+    this.questionsService.checkNewQuestions();
   }
 
   @Cron('0 0 10 * * *')
