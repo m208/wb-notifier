697969f77bef7f3fa362bb9a25febaa7171c2d2f
m208 <avkomissarov@gmail.com>
2024-02-29 00:58:07 +0300

    feat: api links

--- src/constants/apiLinks.ts
+++ src/constants/apiLinks.ts
@@ -1,5 +1,6 @@
 export const wbApiLinks = {
   getOrders: `https://suppliers-api.wildberries.ru/api/v3/orders/new`,
   getProducts: `https://suppliers-api.wildberries.ru/content/v1/cards/filter`,
-  getQuestions: `https://feedbacks-api.wb.ru/api/v1/questions`,
+  getQuestions: `https://feedbacks-api.wildberries.ru/api/v1/questions`,
+  getFeedbacks: `https://feedbacks-api.wildberries.ru/api/v1/feedbacks`,
 };
