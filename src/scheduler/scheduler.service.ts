d88214a6c86767228e33aa10978696e3b1099510
m208 <avkomissarov@gmail.com>
2024-02-29 22:16:11 +0300

    feat: add new feedbacks tracker to sheduler

--- src/scheduler/scheduler.service.ts
+++ src/scheduler/scheduler.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
+import { NewFeedbacksTrackerService } from 'src/new-feedbacks-tracker/new-feedbacks-tracker.service';
 import { NewOrdersTrackerService } from 'src/new-orders-tracker/new-orders-tracker.service';
 import { NewQuestionsService } from 'src/new-questions/new-questions.service';
 import { TokenExpiresTrackerService } from 'src/token-expires-tracker/token-expires-tracker.service';
@@ -11,6 +12,7 @@ export class SchedulerService {
     private readonly trackingService: NewOrdersTrackerService,
     private readonly tokenService: TokenExpiresTrackerService,
     private readonly questionsService: NewQuestionsService,
+    private readonly feedbacksService: NewFeedbacksTrackerService,
   ) {}
 
   @Cron('0 1 * * * *')
@@ -19,10 +21,18 @@ export class SchedulerService {
     this.trackingService.checkNewOrders();
   }
 
+  // ! Allowed 1 request per second for Feedbacks/questions API !
+
   @Cron('0 2 * * * *')
   //  Called every hour, at the start of the 2nd minute
   trackQuestions() {
     this.questionsService.checkNewQuestions();
+  }
+
+  @Cron('0 3 * * * *')
+  //  Called every hour, at the start of the 3rd minute
+  trackFeedbacks() {
+    this.feedbacksService.checkNewFeedbacks();
   }
 
   @Cron('0 0 10 * * *')
