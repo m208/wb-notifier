05546fdc89cf72aa0b1849ee32cb674f2ec9423c
m208 <avkomissarov@gmail.com>
2025-02-11 00:11:34 +0300

    update: API links https://t.me/wb_api_notifications/160

--- src/constants/apiLinks.ts
+++ src/constants/apiLinks.ts
@@ -1,6 +1,6 @@
 export const wbApiLinks = {
-  getOrders: `https://suppliers-api.wildberries.ru/api/v3/orders/new`,
-  getProducts: `https://suppliers-api.wildberries.ru/content/v2/get/cards/list`,
+  getOrders: `https://marketplace-api.wildberries.ru/api/v3/orders/new`,
+  getProducts: `https://content-api.wildberries.ru/content/v2/get/cards/list`,
   getQuestions: `https://feedbacks-api.wildberries.ru/api/v1/questions`,
   getFeedbacks: `https://feedbacks-api.wildberries.ru/api/v1/feedbacks`,
   getClaims: `https://returns-api.wildberries.ru/api/v1/claims`,
